dcbf43e26477d6e2d7d962a28b9bca43d8ccf311
Rafael <c.rafa.14@gmail.com>
2018-05-05 11:02:59 -0400

    asd

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@
 const run = async () =>{
 
   //Creamos la conexión con la BDD correspondiente
-  const mc = await MongoClient.connect('mongodb://heroku_tzlnxnvb:r12345@ds215380.mlab.com:15380/heroku_tzlnxnvb');
+  const mc = await MongoClient.connect('mongodb://publicTransport:1234@ds215380.mlab.com:15380/heroku_tzlnxnvb');
   const mongo: Db = mc.db('library');
   const db = {
     
